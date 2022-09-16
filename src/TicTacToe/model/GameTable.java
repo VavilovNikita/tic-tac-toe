4e675c73b60be1b1c9115cc953f12bcc6592b8e4
VavilovNikita <112397078+VavilovNikita@users.noreply.github.com>
2022-09-16 23:45:24 +0500

    add model classes

--- src/TicTacToe/model/GameTable.java
+++ src/TicTacToe/model/GameTable.java
@@ -1,4 +1,21 @@
 package TicTacToe.model;
 
 public class GameTable {
+
+    private final char[][] table = {
+            {' ',' ',' '},
+            {' ',' ',' '},
+            {' ',' ',' '}};
+
+    public boolean  isEmpty(final Cell cell){
+        return table[cell.getRow()][cell.getCol()] == ' ';
+    }
+
+    public char getSign(final Cell cell){
+        return table[cell.getRow()][cell.getCol()];
+    }
+
+    public void cetSign(final Cell cell, final char sign){
+        table[cell.getRow()][cell.getCol()] = sign;
+    }
 }
