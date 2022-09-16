c774d7c40d22f7a452f42a2af998a1ce3fc90586
VavilovNikita <112397078+VavilovNikita@users.noreply.github.com>
2022-09-16 23:53:19 +0500

    Ovarride toString

--- src/TicTacToe/model/GameTable.java
+++ src/TicTacToe/model/GameTable.java
@@ -1,21 +1,37 @@
 package TicTacToe.model;
 
+import java.util.Arrays;
+
 public class GameTable {
 
     private final char[][] table = {
-            {' ',' ',' '},
-            {' ',' ',' '},
-            {' ',' ',' '}};
+            {' ', ' ', ' '},
+            {' ', ' ', ' '},
+            {' ', ' ', ' '}};
 
-    public boolean  isEmpty(final Cell cell){
+    public boolean isEmpty(final Cell cell) {
         return table[cell.getRow()][cell.getCol()] == ' ';
     }
 
-    public char getSign(final Cell cell){
+    public char getSign(final Cell cell) {
         return table[cell.getRow()][cell.getCol()];
     }
 
-    public void cetSign(final Cell cell, final char sign){
+    public void cetSign(final Cell cell, final char sign) {
         table[cell.getRow()][cell.getCol()] = sign;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("GameTable{");
+        sb.append("table=");
+        for (int i = 0; i < table.length; i++) {
+            sb.append(Arrays.toString(table[i]));
+            if (i < table.length - 1) {
+                sb.append(";");
+            }
+        }
+        sb.append('}');
+        return sb.toString();
     }
 }
